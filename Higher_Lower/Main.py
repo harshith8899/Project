fc915840a18e474c1d89b95945dd67856be40de7
Himateju <himateju21@gmail.com>
2023-11-05 12:30:06 +0530

    Update Main.py

--- Higher_Lower/Main.py
+++ Higher_Lower/Main.py
@@ -7,9 +7,13 @@ from game_data import actors_data
 from art import vs
 from art import logo
 
-def assign():
-    return random.choice(sports_data)
-
+def assign(category):
+    temp = 1
+    if temp == 1:
+        return random.choice(sports_data)
+    elif temp == 2:
+        return random.choice(actors_data)
+    
 def compare(p1,p2,user_ans):
     followers1 = p1['follower_count']
     followers2 = p2['follower_count']
@@ -27,21 +31,24 @@ def compare(p1,p2,user_ans):
         return False
 
 def higher_lower():
-    person1 = assign()
-    person2 = assign()
 
-    while(person1 == person2):
-        person2 = assign()
+    print(logo)
+    category = input("Choose the category\n 1.Sports\n 2.Film\n")
+    person1 = assign(category)
+    person2 = assign(category)
+    #while(person1 == person2):
+    #   person2 = assign()
+
     print(f"Name: {person1['name']}, Desc: {person1['description']}")
     print(vs)
     print(f"Name: {person2['name']}, Desc: {person2['description']}")
 
 user_choice = input("Start Game(y/n)\n")
 
-if user_choice =='y':
+if user_choice == 'y':
     os.system('cls')
     higher_lower()
-elif user_choice =='n':
+elif user_choice == 'n':
     print("Exiting!")
 else: 
     print("Invalid Input.")
